8891be180ca87526aed22322d2130b7fbb4aca1b
MIKE <mikebonus777@gmail.com>
2023-03-06 15:37:11 -0500

    branch-4[commit-2]

--- app/src/main/java/com/luxpmsoft/luxaipoc/view/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/luxpmsoft/luxaipoc/view/activity/splash/SplashActivity.kt
@@ -18,7 +18,7 @@ import com.luxpmsoft.luxaipoc.view.activity.login.SignInActivity
 import com.luxpmsoft.luxaipoc.view.activity.registration.organization.SelectAccountTypeActivity
 import com.luxpmsoft.luxaipoc.view.activity.welcome.WelcomeActivity
 
-// branch-4 (commit1)
+// branch-4 (commit2)
 class SplashActivity : AppCompatActivity() {
 
     companion object{
